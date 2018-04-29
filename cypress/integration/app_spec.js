f81bc89d764863cce3db52079c9552a1475cb5e2
Marko <markthomasnoonan@gmail.com>
2018-04-29 19:33:12 -0400

    add some basic cypress tests

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,5 +1,39 @@
 describe('Mobility ETA', () => {
-	it('should render the homepage', () => {
-		cy.visit('http://localhost:8888/mobility-eta/').contains('Mobility ETA')
+	beforeEach(() => {
+		cy.visit('http://localhost:1234').contains('Mobility ETA')
+	})
+
+	it('renders the homepage', () => {
+		cy.contains('Mobility ETA')
+	})
+
+	it('renders a login form', () => {
+		cy.get('form')
+	})
+
+	it('allows the test user to log in', () => {
+		login()
+	})
+
+	it('displays the trip for the user', () => {
+		login()
+		cy.contains('Trip Status' || 'Ready Time' || 'Booking ID')
+		cy.get('body').should('not.contain', 'undefined')
 	})
 })
+
+function login() {
+	return cy
+		.get('form > label')
+		.contains('Client ID')
+		.parent()
+		.find('input')
+		.type('test')
+		.get('form > label')
+		.contains('Password')
+		.parent()
+		.find('input')
+		.type('test')
+		.get('button')
+		.click()
+}
